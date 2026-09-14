213e802c1242f1578b55200c5e8189251c3e1588
eclypsed212 <eclypse813@gmail.com>
2026-09-14 11:01:20 +0200

    opdracht rekenhuidige operatoren

--- Oefenopdrachten/Opdracht 2/sketch.js
+++ Oefenopdrachten/Opdracht 2/sketch.js
@@ -4,4 +4,29 @@ function setup() {
 
 function draw() {
   background(220);
+
+// cijfer
+let x = 100 
+text ( x, 20,20 );
+
+//variable met text (met const)
+const greeting = "Hello world!"
+text( greeting, 20,60)
+
+// alle variabelen met plus/min/vermenigvuldingen/delen 
+let a = 20; 
+let b = 10; 
+let optellen = a + b;
+let aftrekken = a - b; 
+let vermenigvuldigen = a * b; 
+let delen = a - b;
+
+
+text ("Optellen: " + optellen, 20,80)
+text ("Aftreken: " + aftrekken, 20,100)
+text ("Vermenigvuldigen: " + vermenigvuldigen, 20,120)
+text ("Delen: " + delen, 20, 140)
+
+
+
 }
