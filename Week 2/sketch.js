6f3aeefd1bb7a4c1efa503d47da558514f2d2a93
eclypsed212 <eclypse813@gmail.com>
2026-09-15 15:20:50 +0200

    clouds and trees

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -33,14 +33,22 @@ function draw() {
 
 
     //mountains 
+  stroke(0)
+  strokeWeight(1)
   fill(41,41,41)
   triangle(290, 475, 650, 475, 475, 100)
 
+
+  stroke(0)
+  strokeWeight(1)
   fill(135,135,135)
   triangle(115, 475, 450, 475, 325, 200)
 
+  stroke(0)
+  strokeWeight(1)
   fill(135,135,135)
   triangle(290, 475, 800, 475, 625, 100)
+
   //traffic light
   fill(48)
   rect(650,300,45,120)
@@ -59,14 +67,59 @@ function draw() {
   circle(673,395,30)
   
 
-
-
   //trees
 
 
   for(let i=0; i<4; i++){
     fill(92,56,3)
-    rect(50,50,20,45)
+    rect(45 + i * 120,400,10, 85)
+
+
+
+  for (let i =0; i<4; i++){
+    let sway1 = sin(frameCount * 0.05 + i) * 10;
+    let sway2 = sin(frameCount * 0.07 + i) * 10;
+    let sway3 = sin(frameCount * 0.09 + i) * 10;
+
+    fill(64,173,56)
+    circle(49+i*120 + sway3,400,50)
+
+    fill(40,112,35)
+    circle(49+i*120 + sway2,400,50)
+
+    fill(25,82,21)
+    circle(49+i*120 + sway1,400,50)
+  }
+  //clouds
+
+  fill(255)
+  circle(40,55,50)
+  circle(75,50,65)
+  circle(120,55,50)
+
+  fill(255)
+  circle(55,55,50)
+  circle(90,50,65)
+  circle(135,55,50)
+  
+  
+  
+  
+  //let auto1xpositie = 0;
+  //rect(100 + auto1xpositie,100,140,50)
+  //auto1xpositie = auto1xpositie + 5 
+
+  //if(auto1xpositie>800){
+  //auto1xpositie = - 300
+  //}
+
+  //letricchtingauto1=3
+
+  
+
+
+
+
   }
 
   
