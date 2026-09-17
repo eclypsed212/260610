7ab58180f8c95a1199151e39178b5f2be8f48196
eclypsed212 <eclypse813@gmail.com>
2026-09-17 14:20:19 +0200

    Random function

--- Lege sketch/sketch.js
+++ Lege sketch/sketch.js
@@ -5,5 +5,20 @@ function setup() {
 function draw() {
   background(220);
   
+  let a = random (100);
+  let b = prompt("Write number") ;
+
+  
+
+  if ( a < b ){
+    console.log(a, " is kleiner dan", b)
+  }
+  if (a > b){
+    console.log(a, "is grooter dan", b)
+
+  }
+  if (a == b){
+    console.log( a, "is gelijk aan", b)
+  }
   
 }
