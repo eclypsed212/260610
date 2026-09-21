e4519342faa26754d47bdbf9e18a166eb4694bc2
eclypsed212 <eclypse813@gmail.com>
2026-09-21 12:00:31 +0200

    Update sketch.js

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,7 +1,38 @@
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
   background(220);
+  
+
+  //background
+  fill(122, 4, 122)
+  rect(0,0,400,800)
+  fill(0,30,255)
+  rect(400,0,400,800)
+
+
+  //workplace
+
+
+  fill(0)
+  rect(210,210,400,400,25)
+
+  //buttons
+
+
+
+
+
+  for(i = 0; i > 9; i++ ){
+
+
+
+  }
+  
+  
+  
+  
 }
